262570d43001cb2dc35df138e0c8f15be643f9ac
Alex J. Green <alexjeffreygreen@gmail.com>
2023-04-28 21:06:13 -0400

    Init project setup

---
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    private float masterVolumne;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start audio manager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
